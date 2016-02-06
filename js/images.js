f63e793b4ea3a46b59eced4c741cf3a760ffaa51
Brendan Whiting <brendan.whiting@me.com>
2016-02-05 23:02:01 -0500

    lots of work on modal window

--- js/images.js
+++ js/images.js
@@ -1,80 +1,108 @@
 var images = [
     { 
-        "url": "01.jpg",
+        "idx": "01",
         "title": "Hay Bales",
         "caption": "I love hay bales. Took this snap on a drive through the countryside past some straw fields."
     },
     { 
-        "url": "02.jpg",
+        "idx": "02",
         "title": "Lake",
         "caption": "The lake was so calm today. We had a great view of the snow on the mountains from here."
     },
     { 
-        "url": "03.jpg",
+        "idx": "03",
         "title": "Canyon",
         "caption": "I hiked to the top of the mountain and got this picture of the canyon and trees below."
     },
         { 
-        "url": "04.jpg",
+        "idx": "04",
         "title": "Iceberg",
         "caption": "It was amazing to see an iceberg up close, it was so cold but didn’t snow today."
     },
         { 
-        "url": "05.jpg",
+        "idx": "05",
         "title": "Desert",
         "caption": "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
     },
         { 
-        "url": "06.jpg",
+        "idx": "06",
         "title": "Fall",
         "caption": "Fall is coming, I love when the leaves on the trees start to change color."
     },
         { 
-        "url": "07.jpg",
+        "idx": "07",
         "title": "Plantation",
         "caption": "I drove past this plantation yesterday, everything is so green!"
     },
         { 
-        "url": "08.jpg",
+        "idx": "08",
         "title": "Dunes",
         "caption": "My summer vacation to the Oregon Coast. I love the sandy dunes!"
     },
         { 
-        "url": "09.jpg",
+        "idx": "09",
         "title": "Countryside Lane",
         "caption": "We enjoyed a quiet stroll down this countryside lane."
     },
         { 
-        "url": "10.jpg",
+        "idx": "10",
         "title": "Sunset",
         "caption": "Sunset at the coast! The sky turned a lovely shade of orange."
     },
         { 
-        "url": "11.jpg",
+        "idx": "11",
         "title": "Cave",
         "caption": "I did a tour of a cave today and the view of the landscape below was breathtaking."
     },
         { 
-        "url": "12.jpg",
+        "idx": "12",
         "title": "Bluebells",
         "caption": "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
     },
 ]
 
+function bind_model_to_imgs() {
+    var imgs = document.getElementsByTagName("img");
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].onclick = function() {
+            var modal_window = document.getElementById("modal-window");
+            modal_window.style.opacity="0.9";
+            modal_window.style.display="block";
+            var content_wrapper = document.getElementById("content-wrapper");
+            console.log(content_wrapper)
+            var html_string = "<img src='images/full-pictures/idx.jpg'\
+                                class='modal-image'\
+                                alt='title'>\
+                                <p class='cpn-text'>caption</p>"
+
+            html_string = html_string.replace("title", this.getAttribute("title"));
+            html_string = html_string.replace("idx", this.getAttribute("data_index"));
+            html_string = html_string.replace("caption", this.getAttribute("data_caption"));
+
+
+
+            content_wrapper.innerHTML = html_string;
+            console.log("I clicked an image");
+        }
+    }
+    console.log(imgs);
+}
+
 function populate_images(image_list) {
     var image_container = document.getElementById("image-container");
     image_container.innerHTML = "";
-
-    console.log(image_container);
     for (var i = 0; i < image_list.length; i++) {
-        var html_string = "<div class='image'><a href='images/full-pictures/url'>\
-                          <img src='images/thumbnails/url' alt='title'></a></div>";
+        var html_string = "<div class='image'><img src='images/thumbnails/idx.jpg'\
+                            data_index='idx'\
+                            data_caption='cpn'\
+                            alt='title'></div>";
         html_string = html_string.replace("title", image_list[i].title);
-        html_string = html_string.replace("url", image_list[i].url); 
-        html_string = html_string.replace("url", image_list[i].url); 
+        html_string = html_string.replace("idx", image_list[i].idx); 
+        html_string = html_string.replace("idx", image_list[i].idx); 
+        html_string = html_string.replace("cpn", image_list[i].caption); 
         image_container.innerHTML += html_string;
-
     } 
+    bind_model_to_imgs();
 }
 
 function bind_input_event() {
@@ -94,8 +122,8 @@ function bind_input_event() {
     }
 }
 
-
 window.onload=function() {
     populate_images(images);
     bind_input_event();
+    bind_model_to_imgs();
 }
